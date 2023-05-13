82b6f39b7977999be2294e78d40c645cd5b63624
harshmangalam <harshdev8218@gmail.com>
2023-05-13 19:49:34 +0530

    added shopping cart

---
+++ src/routes/carts/cart-item.tsx
@@ -0,0 +1,41 @@
+import { component$ } from "@builder.io/qwik";
+
+export const CartItem = component$(() => {
+  return (
+    <li class="flex py-6">
+      <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+        <img
+          src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg"
+          alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
+          class="h-full w-full object-cover object-center"
+          width="100%"
+          height="100%"
+        />
+      </div>
+
+      <div class="ml-4 flex flex-1 flex-col">
+        <div>
+          <div class="flex justify-between text-base font-medium text-gray-900">
+            <h3>
+              <a href="#">Throwback Hip Bag</a>
+            </h3>
+            <p class="ml-4">$90.00</p>
+          </div>
+          <p class="mt-1 text-sm text-gray-500">Salmon</p>
+        </div>
+        <div class="flex flex-1 items-end justify-between text-sm">
+          <p class="text-gray-500">Qty 1</p>
+
+          <div class="flex">
+            <button
+              type="button"
+              class="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+});
---
+++ src/routes/carts/index.tsx
@@ -0,0 +1,55 @@
+import { component$ } from "@builder.io/qwik";
+import { CartItem } from "./cart-item";
+
+export default component$(() => {
+  return (
+    <div class="max-w-2xl w-full mx-auto shadow border">
+      <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+        <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">
+          Shopping cart
+        </h2>
+
+        <div class="mt-8">
+          <ul role="list" class="-my-6 divide-y divide-gray-200">
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </ul>
+        </div>
+      </div>
+
+      <div class="border-t border-gray-200 px-4 py-6 sm:px-6 sticky bottom-0 bg-white">
+        <div class="flex justify-between text-base font-medium text-gray-900">
+          <p>Subtotal</p>
+          <p>$262.00</p>
+        </div>
+        <p class="mt-0.5 text-sm text-gray-500">
+          Shipping and taxes calculated at checkout.
+        </p>
+        <div class="mt-6">
+          <a
+            href="#"
+            class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+          >
+            Checkout
+          </a>
+        </div>
+        <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
+          <p>
+            or
+            <button
+              type="button"
+              class="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Continue Shopping
+              <span aria-hidden="true"> &rarr;</span>
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+});
