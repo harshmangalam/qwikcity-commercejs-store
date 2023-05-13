ca593ba5cf2ea586d5c19f828f750a06fcbb0995
harshmangalam <harshdev8218@gmail.com>
2023-05-13 15:55:54 +0530

    show lists of product

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,65 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { ProductItem } from "~/components/product-item";
 
 export default component$(() => {
+  const products = [
+    {
+      id: 1,
+      name: "Earthen Bottle",
+      href: "#",
+      price: "$48",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+      imageAlt:
+        "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+    },
+    {
+      id: 2,
+      name: "Nomad Tumbler",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+      imageAlt:
+        "Olive drab green insulated bottle with flared screw lid and flat top.",
+    },
+    {
+      id: 3,
+      name: "Focus Paper Refill",
+      href: "#",
+      price: "$89",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+      imageAlt:
+        "Person using a pen to cross a task off a productivity paper card.",
+    },
+    {
+      id: 4,
+      name: "Machined Mechanical Pencil",
+      href: "#",
+      price: "$35",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+      imageAlt:
+        "Hand holding black machined steel mechanical pencil with brass tip and top.",
+    },
+  ];
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <div class="bg-white">
+      <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 class="sr-only">Products</h2>
+
+        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <ProductItem key={product.id} {...product} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
-  meta: [
-    {
-      name: 'description',
-      content: 'Qwik site description',
-    },
-  ],
+  title: "Home",
 };
