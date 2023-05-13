7c2506d673b99e48e9804a53da6c1d7e97ecc69e
harshmangalam <harshdev8218@gmail.com>
2023-05-13 20:04:53 +0530

    updated product overview layout

--- src/routes/products/[slug]/index.tsx
+++ src/routes/products/[slug]/index.tsx
@@ -27,42 +27,30 @@ export default component$(() => {
   const productLoader = useProduct();
   const product = productLoader.value.product;
   return (
-    <div class="bg-white">
-      <div class="pt-6">
-        <ImageGallery />
+    <div class="grid max-w-5xl mx-auto w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+      <div class="rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-6">
+        <img
+          src={product.imageSrc}
+          alt={product.imageAlt}
+          class="object-cover object-center"
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <div class="sm:col-span-8 lg:col-span-6 flex flex-col space-y-4">
+        <h2 class="text-2xl font-bold text-gray-900">{product.name}</h2>
+        <p class="text-2xl text-gray-900">{product.price}</p>
 
-        <div class="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
-          <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-              {product.name}
-            </h1>
-          </div>
+        <button
+          type="submit"
+          class="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Add to cart
+        </button>
 
-          <div class="mt-4 lg:row-span-3 lg:mt-0">
-            <h2 class="sr-only">Product information</h2>
-            <p class="text-3xl tracking-tight text-gray-900">
-              ${product.price}
-            </p>
-
-            <form class="mt-10">
-              <button
-                type="submit"
-                class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Add to bag
-              </button>
-            </form>
-          </div>
-
-          <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-            <div>
-              <h3 class="sr-only">Description</h3>
-
-              <div class="space-y-6">
-                <p class="text-base text-gray-900">{product.description}</p>
-              </div>
-            </div>
-          </div>
+        <div class="flex flex-col space-y-2">
+          <h4 class="text-lg">Description</h4>
+          <p class=" text-gray-600">{product.description}</p>
         </div>
       </div>
     </div>
