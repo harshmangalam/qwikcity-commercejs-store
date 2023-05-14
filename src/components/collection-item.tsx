5bd0d0f6a6e1bd2d0e9af70c3a4530cc258cd570
harshmangalam <harshdev8218@gmail.com>
2023-05-14 08:42:07 +0530

    fetch collectios from commerce sdk

--- src/components/collection-item.tsx
+++ src/components/collection-item.tsx
@@ -1,27 +1,29 @@
 import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
-
-export const CollectionItem = component$(() => {
+interface CollectionItemProps {
+  imageSrc: string;
+  name: string;
+  slug: string;
+}
+export const CollectionItem = component$((props: CollectionItemProps) => {
+  const { imageSrc, name, slug } = props;
   return (
     <div class="group relative">
       <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
         <img
-          src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg"
-          alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+          src={imageSrc}
+          alt={name}
           class="h-full w-full object-cover object-center"
           width={"100%"}
           height={"100%"}
         />
       </div>
-      <h3 class="mt-6 text-sm text-gray-500">
-        <Link href="/products/?collection=desk-and-office">
+      <h3 class="mt-2 font-semibold text-center">
+        <Link href={`/products/?collection=${slug}`}>
           <span class="absolute inset-0"></span>
-          Desk and Office
+          {name}
         </Link>
       </h3>
-      <p class="text-base font-semibold text-gray-900">
-        Work from home accessories
-      </p>
     </div>
   );
 });
--- src/routes/collections/index.tsx
+++ src/routes/collections/index.tsx
@@ -1,18 +1,26 @@
 import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import { CollectionItem } from "~/components/collection-item";
+import commerce from "~/lib/commerce";
 
+export const useCollections = routeLoader$(async () => {
+  const collections = await commerce.categories.list();
+  return collections;
+});
 export default component$(() => {
+  const collectionsLoader = useCollections();
+
   return (
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
-        <CollectionItem />
-        <CollectionItem />
-        <CollectionItem />
-        <CollectionItem />
-        <CollectionItem />
-        <CollectionItem />
-        <CollectionItem />
-        <CollectionItem />
+        {collectionsLoader?.value?.data?.map((collection) => (
+          <CollectionItem
+            key={collection.id}
+            name={collection.name}
+            slug={collection.slug}
+            imageSrc={(collection as any).assets[0]?.url}
+          />
+        ))}
       </div>
     </div>
   );
