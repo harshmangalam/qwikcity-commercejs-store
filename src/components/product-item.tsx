955a1ae4d10f069ee1e0ae1749104daf1bbe0f24
harshmangalam <harshdev8218@gmail.com>
2023-05-14 09:00:38 +0530

    fetch products from commerce sdk

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -2,20 +2,19 @@ import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 
 interface ProductItemProps {
-  id: any;
   imageSrc: string;
-  imageAlt: string;
   name: string;
   price: string;
+  sku: string;
 }
 export const ProductItem = component$((props: ProductItemProps) => {
-  const { id, imageAlt, imageSrc, name, price } = props;
+  const { sku, imageSrc, name, price } = props;
   return (
-    <Link key={id} href={`/products/1`} class="group">
+    <Link href={`/products/${sku}`} class="group">
       <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <img
           src={imageSrc}
-          alt={imageAlt}
+          alt={name}
           class="h-full w-full object-cover object-center group-hover:opacity-75"
           width={"100%"}
           height={"100%"}
--- src/routes/products/index.tsx
+++ src/routes/products/index.tsx
@@ -1,95 +1,40 @@
 import { component$ } from "@builder.io/qwik";
 import { type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
+import { type ProductCollection } from "@chec/commerce.js/features/products";
 import { ProductItem } from "~/components/product-item";
+import commerce from "~/lib/commerce";
 
-export const useProducts = routeLoader$(() => {
-  const products = [
-    {
-      id: 1,
-      name: "Earthen Bottle",
-      price: "$48",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-      imageAlt:
-        "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-    },
-    {
-      id: 2,
-      name: "Nomad Tumbler",
-      price: "$35",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-      imageAlt:
-        "Olive drab green insulated bottle with flared screw lid and flat top.",
-    },
-    {
-      id: 3,
-      name: "Focus Paper Refill",
-      price: "$89",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
-      imageAlt:
-        "Person using a pen to cross a task off a productivity paper card.",
-    },
-    {
-      id: 4,
-      name: "Machined Mechanical Pencil",
-      price: "$35",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
-      imageAlt:
-        "Hand holding black machined steel mechanical pencil with brass tip and top.",
-    },
-    {
-      id: 5,
-      name: "Earthen Bottle",
-      price: "$48",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-      imageAlt:
-        "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-    },
-    {
-      id: 6,
-      name: "Nomad Tumbler",
-      price: "$35",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-      imageAlt:
-        "Olive drab green insulated bottle with flared screw lid and flat top.",
-    },
-    {
-      id: 7,
-      name: "Focus Paper Refill",
-      price: "$89",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
-      imageAlt:
-        "Person using a pen to cross a task off a productivity paper card.",
-    },
-    {
-      id: 8,
-      name: "Machined Mechanical Pencil",
-      price: "$35",
-      imageSrc:
-        "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
-      imageAlt:
-        "Hand holding black machined steel mechanical pencil with brass tip and top.",
-    },
-  ];
+export const useProducts = routeLoader$(async ({ query }) => {
+  const collectionSlug = query.get("collection");
+  let products: ProductCollection;
 
-  return {
-    products,
-  };
+  if (collectionSlug) {
+    products = await commerce.products.list({
+      category_slug: collectionSlug,
+    });
+  } else {
+    products = await commerce.products.list();
+    return products;
+  }
+
+  return products;
 });
 export default component$(() => {
-  const loader = useProducts();
-  const products = loader.value.products;
+  const productsLoader = useProducts();
+  const products = productsLoader.value.data;
   return (
     <div class="mx-auto max-w-6xl w-full">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {products.map((product) => (
-          <ProductItem key={product.id} {...product} />
+        {products === undefined ||
+          (products?.length === 0 && <p>No products</p>)}
+        {products?.map((product) => (
+          <ProductItem
+            key={product.id}
+            sku={product.sku as string}
+            name={product.name}
+            imageSrc={product.assets[0].url}
+            price={product.price.formatted_with_symbol}
+          />
         ))}
       </div>
     </div>
