13c664c65809faf6ceee88d2e51f96191c171d39
harshmangalam <harshdev8218@gmail.com>
2023-05-14 10:54:09 +0530

    updated product item

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -11,15 +11,14 @@ export const ProductItem = component$((props: ProductItemProps) => {
   const { id, imageSrc, name, price } = props;
   return (
     <Link href={`/products/${id}`} class="group">
-      <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-        <img
-          src={imageSrc}
-          alt={name}
-          class="h-full w-full object-cover object-center group-hover:opacity-75"
-          width={"100%"}
-          height={"100%"}
-        />
-      </div>
+      <img
+        src={imageSrc}
+        alt={name}
+        class="w-full h-80 object-cover object-center group-hover:opacity-75"
+        width={"100%"}
+        height={"100%"}
+      />
+
       <h3 class="mt-4 text-sm text-gray-700">{name}</h3>
       <p class="mt-1 text-lg font-medium text-gray-900">{price}</p>
     </Link>
