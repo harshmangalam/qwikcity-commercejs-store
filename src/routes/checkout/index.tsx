368a4b03a08bd8b4848f89691c558b51481d32fd
harshmangalam <harshdev8218@gmail.com>
2023-05-16 18:57:39 +0530

    added checkout

--- src/routes/checkout/index.tsx
+++ src/routes/checkout/index.tsx
@@ -4,6 +4,7 @@ import {
   noSerialize,
   useSignal,
   useVisibleTask$,
+  $,
 } from "@builder.io/qwik";
 import {
   routeLoader$,
@@ -55,36 +56,40 @@ export const useCreateOrder = routeAction$(
     stripePaymentId,
     checkoutTokenId,
   }) => {
-    const token = await commerce.checkout.getToken(checkoutTokenId);
+    try {
+      const token = await commerce.checkout.getToken(checkoutTokenId);
 
-    const order = await commerce.checkout.capture(checkoutTokenId, {
-      line_items: token.live.line_items,
-      customer: {
-        email,
-        firstname: firstName,
-        lastname: lastName,
-        phone,
-      },
-      shipping: {
-        name: "Primary",
-        street: address,
-        country,
-        town_city: city,
-        postal_zip_code: pinCode,
-        county_state: state,
-      },
-      fulfillment: {
-        shipping_method: "",
-      },
-      payment: {
-        gateway: "stripe",
-        stripe: {
-          payment_method_id: stripePaymentId,
+      const order = await commerce.checkout.capture(checkoutTokenId, {
+        line_items: token.live.line_items,
+        customer: {
+          email,
+          firstname: firstName,
+          lastname: lastName,
+          phone,
         },
-      },
-    });
+        shipping: {
+          name: "Primary",
+          street: address,
+          country,
+          town_city: city,
+          postal_zip_code: pinCode,
+          county_state: state,
+        },
+        fulfillment: {
+          shipping_method: "",
+        },
+        payment: {
+          gateway: "stripe",
+          stripe: {
+            payment_method_id: stripePaymentId,
+          },
+        },
+      });
 
-    console.log(order);
+      console.log(order);
+    } catch (error) {
+      console.log(error);
+    }
   },
   zod$({
     email: z.string(),
@@ -111,6 +116,9 @@ export const useCheckout = routeLoader$(async () => {
 });
 
 export default component$(() => {
+  const stripePaymentId = useSignal("");
+  const loading = useSignal(false);
+  const current = useSignal<"payment" | "other">("payment");
   const servicesLoader = useListServices();
   const countries = servicesLoader.value.countries;
   const subdivisonsLoader = useListSubDivisions();
@@ -145,6 +153,26 @@ export default component$(() => {
     }
   });
 
+  const handlePayment = $(async () => {
+    loading.value = true;
+    try {
+      const result = await stripe.value?.createPaymentMethod({
+        type: "card",
+        card: cardElement.value as StripeCardElement,
+      });
+
+      console.log(result);
+      if (result?.paymentMethod?.id) {
+        stripePaymentId.value = result?.paymentMethod?.id;
+        current.value = "other";
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      loading.value = false;
+    }
+  });
+
   return (
     <Form
       action={createOrderAction}
@@ -155,105 +183,118 @@ export default component$(() => {
         name="checkoutTokenId"
         value={checkoutLoader.value.id}
       />
+      <input
+        type="hidden"
+        name="stripePaymentId"
+        value={stripePaymentId.value}
+      />
       <div>
-        <div class="grid grid-cols-2 gap-4">
-          <label for="email" class="flex flex-col space-y-2 col-span-2">
-            <span class="text-gray-700">Email address</span>
-            <input
-              class="w-full rounded-md"
-              type="email"
-              name="email"
-              id="email"
-            />
-          </label>
+        <div>
+          {current.value === "other" && (
+            <div class="grid grid-cols-2 gap-4">
+              <label for="email" class="flex flex-col space-y-2 col-span-2">
+                <span class="text-gray-700">Email address</span>
+                <input
+                  class="w-full rounded-md"
+                  type="email"
+                  name="email"
+                  id="email"
+                />
+              </label>
 
-          <label for="firstName" class="flex flex-col space-y-2">
-            <span class="text-gray-700">First name</span>
-            <input
-              class="w-full rounded-md"
-              type="text"
-              name="firstName"
-              id="firstName"
-            />
-          </label>
-          <label for="lastName" class="flex flex-col space-y-2">
-            <span class="text-gray-700">Last name</span>
-            <input
-              class="w-full rounded-md"
-              type="text"
-              name="lastName"
-              id="lastName"
-            />
-          </label>
-          <label for="address" class="flex flex-col space-y-2 col-span-2">
-            <span class="text-gray-700">Address</span>
-            <textarea
-              class="w-full rounded-md"
-              rows={4}
-              name="address"
-              id="address"
-            />
-          </label>
+              <label for="firstName" class="flex flex-col space-y-2">
+                <span class="text-gray-700">First name</span>
+                <input
+                  class="w-full rounded-md"
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                />
+              </label>
+              <label for="lastName" class="flex flex-col space-y-2">
+                <span class="text-gray-700">Last name</span>
+                <input
+                  class="w-full rounded-md"
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                />
+              </label>
+              <label for="address" class="flex flex-col space-y-2 col-span-2">
+                <span class="text-gray-700">Address</span>
+                <textarea
+                  class="w-full rounded-md"
+                  rows={4}
+                  name="address"
+                  id="address"
+                />
+              </label>
 
-          <label for="country" class="flex flex-col space-y-2">
-            <span class="text-gray-700">Country</span>
-            <select
-              name="country"
-              id="country"
-              disabled={subdivisonsLoader.isRunning}
-              onChange$={(e) =>
-                subdivisonsLoader.submit({ countryCode: e.target.value })
-              }
-            >
-              {Object.keys(countries).map((key) => (
-                <option key={key} value={key} selected={key === "IN"}>
-                  {countries[key]}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label for="state" class="flex flex-col space-y-2">
-            <span class="text-gray-700">State</span>
-            <select name="state" id="state">
-              {Object.keys(states).map((key) => (
-                <option key={key} value={key} selected={key === "BR"}>
-                  {states[key]}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label for="city" class="flex flex-col space-y-2">
-            <span class="text-gray-700">City</span>
-            <input
-              class="w-full rounded-md"
-              type="text"
-              name="city"
-              id="city"
-            />
-          </label>
-          <label for="pinCode" class="flex flex-col space-y-2">
-            <span class="text-gray-700">Pin code</span>
-            <input
-              class="w-full rounded-md"
-              type="text"
-              name="pinCode"
-              id="pinCode"
-            />
-          </label>
-          <label for="phone" class="flex flex-col space-y-2 col-span-2">
-            <span class="text-gray-700">Phone</span>
-            <input
-              class="w-full rounded-md"
-              type="tel"
-              name="phone"
-              id="phone"
-            />
-          </label>
+              <label for="country" class="flex flex-col space-y-2">
+                <span class="text-gray-700">Country</span>
+                <select
+                  name="country"
+                  id="country"
+                  disabled={subdivisonsLoader.isRunning}
+                  onChange$={(e) =>
+                    subdivisonsLoader.submit({ countryCode: e.target.value })
+                  }
+                >
+                  {Object.keys(countries).map((key) => (
+                    <option key={key} value={key} selected={key === "IN"}>
+                      {countries[key]}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label for="state" class="flex flex-col space-y-2">
+                <span class="text-gray-700">State</span>
+                <select name="state" id="state">
+                  {Object.keys(states).map((key) => (
+                    <option key={key} value={key} selected={key === "BR"}>
+                      {states[key]}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label for="city" class="flex flex-col space-y-2">
+                <span class="text-gray-700">City</span>
+                <input
+                  class="w-full rounded-md"
+                  type="text"
+                  name="city"
+                  id="city"
+                />
+              </label>
+              <label for="pinCode" class="flex flex-col space-y-2">
+                <span class="text-gray-700">Pin code</span>
+                <input
+                  class="w-full rounded-md"
+                  type="text"
+                  name="pinCode"
+                  id="pinCode"
+                />
+              </label>
+              <label for="phone" class="flex flex-col space-y-2 col-span-2">
+                <span class="text-gray-700">Phone</span>
+                <input
+                  class="w-full rounded-md"
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                />
+              </label>
+            </div>
+          )}
 
-          <label for="phone" class="flex flex-col space-y-2 col-span-2">
-            <span class="text-gray-700">Payment</span>
-            <div ref={elementRef}>Stripe</div>
-          </label>
+          {current.value === "payment" && (
+            <div>
+              <label for="phone" class="flex flex-col space-y-2 col-span-2">
+                <span class="text-gray-700">Payment</span>
+                <div ref={elementRef}>Stripe</div>
+              </label>
+            </div>
+          )}
         </div>
       </div>
       <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6 shadow border rounded-md">
@@ -284,11 +325,20 @@ export default component$(() => {
           </ul>
 
           <div class="mt-6">
-            <Button
-              label={`Pay ${checkoutLoader.value.live.subtotal.formatted_with_symbol}`}
-              isLoading={createOrderAction.isRunning}
-              type="submit"
-            />
+            {current.value === "payment" ? (
+              <Button
+                isLoading={loading.value}
+                type="button"
+                label="Next"
+                onClick$={handlePayment}
+              />
+            ) : (
+              <Button
+                label={`Pay ${checkoutLoader.value.live.subtotal.formatted_with_symbol}`}
+                isLoading={createOrderAction.isRunning}
+                type="submit"
+              />
+            )}
           </div>
         </div>
       </div>
